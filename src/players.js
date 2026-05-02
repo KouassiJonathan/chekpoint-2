ce6d188de95a154cbc7a227b0cb97b3066427d5d
Jonathan Kouassi <tonemail@gmail.com>
2026-05-02 11:58:38 +0000

    first commit

---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentin",
+    jerseyNumber: 10,
+    age: 38,
+    imageUrl: "https://example.com/messi.jpg"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al Nassr",
+    nationality: "Portugais",
+    jerseyNumber: 7,
+    age: 41,
+    imageUrl: "https://example.com/ronaldo.jpg"
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "PSG",
+    nationality: "Français",
+    jerseyNumber: 7,
+    age: 27,
+    imageUrl: "https://example.com/mbappe.jpg"
+  },
+  {
+    name: "Didier Drogba",
+    team: "Retraité",
+    nationality: "Ivoirien",
+    jerseyNumber: 11,
+    age: 48,
+    imageUrl: "https://example.com/drogba.jpg"
+  }
+];
+
+export default players;
